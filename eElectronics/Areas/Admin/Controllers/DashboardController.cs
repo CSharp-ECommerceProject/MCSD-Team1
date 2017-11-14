5b25cd3da08a6cac3fd5776b059389513a57e688
Ahmed Khaled <ahmed.khaled.119@hotmail.com>
2017-11-14 21:46:01 +0200

    Admin Area

---
+++ eElectronics/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eElectronics.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Admin/Dashboard/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Member()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult Order()
+        {
+            return View();
+        }
+	}
+}
